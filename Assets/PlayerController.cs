a6941275be7cd25e2a055240abe8e735a57a624a
sagibinco <34470750+sagibinco@users.noreply.github.com>
2017-12-25 15:18:06 +0200

    unity
    unity

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D rb2d;
+    void Start()
+    {
+        //Get and store a reference to the Rigidbody2D component so that we can access it.
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        Vector2 start = transform.position;
+        Vector2 end = start + new Vector2(moveHorizontal*speed, moveVertical*speed);
+        
+        if (end.x < 7.8 && end.y < 7.22 && end.x > -7.15 && end.y > -7.09)
+        {
+            rb2d.MovePosition(end);
+        }
+
+
+    }
+}
